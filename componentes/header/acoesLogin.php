b62b1c467fcad171d2ddd8b4edfdabd07da06648
ANA BEATRIZ PEREIRA DE BRITO <21192891@CFP127ALUNO.LOCAL>
2021-10-29 07:54:36 -0300

    save

--- componentes/header/acoesLogin.php
+++ componentes/header/acoesLogin.php
@@ -1,25 +1,11 @@
 <?php
 
+    session_start();
+
     require('../../database/conexao.php');
 
-            function realizarLogin($usuario, $senha, $conexao){
+    
 
-                $sql = "SELECT * FROM tbl_administrador
-                        WHERE usuario = '$usuario' AND senha = '$senha'";
-
-                $resultado = mysqli_query($conexao, $sql);
-
-                $dadosUsuario = mysqli_fetch_array($resultado);
-
-                if (isset($dadosUsuario["usuario"]) && isset($dadosUsuario["senha"])) {
-                    
-                    echo 'LOGADO!';
-
-                }else {
-                    echo 'ALGO DEU ERRADO!!';
-                }
-
-            }
-            realizarLogin('anab', '05270225', $conexao)
+           
 
 ?>
